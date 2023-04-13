5359fe9d16cdfa10e71ded44f5dacaea46682555
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2023-04-13 17:00:00 +0200

    Studio ahora es un record

--- src/fp/anime/test/TestFactoriaAnimes.java
+++ src/fp/anime/test/TestFactoriaAnimes.java
@@ -3,6 +3,7 @@ package fp.anime.test;
 import fp.anime.AnimesImpl;
 import fp.anime.FactoriaAnimesImpl;
 import fp.common.Source;
+import fp.common.Studio;
 
 public class TestFactoriaAnimes {
 	
@@ -14,11 +15,12 @@ public class TestFactoriaAnimes {
 		System.out.println("\nTestLeerAnimes =============");
 		AnimesImpl animes = FactoriaAnimesImpl.leerAnimes(fichero);
 		//System.out.println("   Animes: "+ animes);
-		String genero = "Adventures";
-		System.out.println("Animes de género "+genero+":"+animes.getAnimesGenero(genero));
+		//String genero = "Adventure";
+		//System.out.println("Animes de género "+genero+":"+animes.getAnimesGenero(genero));
 		//System.out.println("Media de valoración de todos los animes: "+animes.mediaValoracionAnimes());
 		//System.out.println("Animes originales:"+animes.getAnimesPorOrigen().get(Source.ORIGINAL));
 		//System.out.println("Número de animes por estudio: "+animes.getNumeroAnimesPorEstudio());
+	
 	}
 
 }
--- src/fp/common/Studio.java
+++ src/fp/common/Studio.java
@@ -1,15 +1,8 @@
 package fp.common;
 
-import java.util.Objects;
-
 import fp.utiles.Checkers;
 
-public class Studio {
-	// Propiedades básicas
-	
-	private Integer id;
-	private String nombre;
-	
+public record Studio (Integer id, String nombre){
 	// Constructor
 	
 	public Studio(Integer id, String nombre) {
@@ -20,43 +13,5 @@ public class Studio {
 		Checkers.checkVacio(nombre);
 		Checkers.checkId(id);
 	}
-	
-	
-	// Getters y setters
-	
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	// Criterio de igualdad
-	
-	public int hashCode() {
-		return Objects.hash(id, nombre);
-	}
-
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Studio other = (Studio) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre);
-	}
-
-	public String toString() {
-		return "Studio [id=" + id + ", nombre=" + nombre + "]";
-	}
-	
-	
 	
 }
