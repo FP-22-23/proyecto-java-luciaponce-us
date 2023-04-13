2edbf72124e45eaa9b8cb7b0bd2ada30145e67ea
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2023-04-14 01:04:43 +0200

    Entrega 2 v3

--- src/fp/anime/AnimesImpl.java
+++ src/fp/anime/AnimesImpl.java
@@ -80,24 +80,11 @@ public class AnimesImpl implements Animes{
 	// Métodos con tratamientos secuenciales
 	
 	public Boolean existeAnimeValoraciónMayorA(Double valoracion) {
-		/*Boolean res = false;
-		for (Anime a:animes) {
-			if (a.getValoracion()>valoracion) {
-				res = true;
-			}
-		}
-		return res;*/
 		return animes.stream()
 				.anyMatch(x -> x.getValoracion()>valoracion);
 	}
 	
 	public Double mediaValoracionAnimes() {
-		/*Double suma = 0.0;
-		for (Anime a : animes) {
-			suma+=a.getValoracion();
-		}
-		Double media = suma/getNumeroAnimes();
-		return media;*/
 		return animes.stream()
 				.mapToDouble(x->x.getValoracion())
 				.average()
@@ -105,17 +92,6 @@ public class AnimesImpl implements Animes{
 	}
 	
 	public List<Anime> getAnimesGenero(String genero){
-		/*if (gs.contains(genero)) {
-			List<Anime> res = new ArrayList<Anime>();
-			for (Anime a : animes) {
-				if (a.getGenero().contains(genero)) {
-					res.add(a);
-				}
-			}
-			return res;
-		}else {
-			throw new IllegalArgumentException("El género debe ser uno de los siguientes:"+gs);
-		}*/
 		Checkers.checkGenero(genero);
 		return animes.stream()
 				.filter(x -> x.getGenero().contains(genero))
@@ -124,31 +100,11 @@ public class AnimesImpl implements Animes{
 	}
 	
 	public Map<Source,List<Anime>> getAnimesPorOrigen(){
-		/*Map<Source,List<Anime>> res = new HashMap<Source,List<Anime>>();
-		for (Anime a : animes) {
-			if (res.containsKey(a.getOrigen())) {
-				res.get(a.getOrigen()).add(a);
-			}else {
-				List<Anime> animes = new ArrayList<Anime>();
-				res.put(a.getOrigen(),animes);
-				animes.add(a);
-			}
-		}
-		return res;*/
 		return animes.stream()
 				.collect(Collectors.groupingBy(Anime::getOrigen,Collectors.toList()));
 	}
 	
 	public Map<Studio,Integer> getNumeroAnimesPorEstudio(){
-		/*Map<Studio,Integer> res = new HashMap<Studio,Integer>();
-		for (Anime a : animes) {
-			if (res.containsKey(a.getEstudio())) {
-				res.put(a.getEstudio(),res.get(a.getEstudio())+1);
-			}else{
-				res.put(a.getEstudio(), 1);
-			}
-		}
-		return res;*/
 		return animes.stream()
 				.collect(
 						Collectors.groupingBy(x -> x.getEstudio(), Collectors.summingInt(e->1))
