c2558b8ef19c59a506f0015c3de49d32e98e0cb8
Lucía Ponce García de Sola <lucpongar@alum.us.es>
2023-05-16 01:33:58 +0200

    Entrega 3 v1

--- src/fp/anime/AnimesStreamImpl.java
+++ src/fp/anime/AnimesStreamImpl.java
@@ -2,10 +2,14 @@ package fp.anime;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import fp.common.Source;
 import fp.common.Studio;
@@ -23,6 +27,10 @@ public class AnimesStreamImpl implements Animes{
 	
 	public AnimesStreamImpl(Collection<Anime> animes) {
 		this.animes = new ArrayList<Anime>(animes);
+	}
+	
+	public AnimesStreamImpl(Stream<Anime> animes) {
+		this.animes = animes.collect(Collectors.toList());
 	}
 	
 	// Criterio de igualdad
@@ -97,10 +105,34 @@ public class AnimesStreamImpl implements Animes{
 	
 	}
 	
+	// Maximo o minimo con filtrado
+	
+	public Anime animeMasPopularDelGenero(String genero) {
+		Checkers.checkGenero(genero);
+		return animes.stream()
+				.filter(x -> x.getGenero().contains(genero))
+				.max(Comparator.comparing(Anime::getPopularidad))
+				.get();
+	}
+	
+	// Una selección, con filtrado y ordenación.
+	
+	public List<Anime> topNAnimesMasPopularesDelGenero(Integer n, String genero){
+		Comparator<Anime> cp = Comparator.comparing(Anime::getPopularidad).reversed();
+		Checkers.checkGenero(genero);
+		return animes.stream()
+				.filter(x -> x.getGenero().contains(genero))
+				.sorted(cp)
+				.limit(n)
+				.collect(Collectors.toList());
+	}
+	
 	public Map<Source,List<Anime>> getAnimesPorOrigen(){
 		return animes.stream()
 				.collect(Collectors.groupingBy(Anime::getOrigen,Collectors.toList()));
 	}
+	
+	// Uno de los métodos (5) implementados en la entrega 2, pero con streams.
 	
 	public Map<Studio,Integer> getNumeroAnimesPorEstudio(){
 		return animes.stream()
@@ -108,5 +140,78 @@ public class AnimesStreamImpl implements Animes{
 						Collectors.groupingBy(x -> x.getEstudio(), Collectors.summingInt(e->1))
 						);
 	}
+	
+	// Un método en cuya implementación se use, o bien el Collector collectingAndThen, o bien el Collector mapping.
+	
+	public Map<Source,Integer> calcularNumeroAnimesPorOrigen(){
+		return animes.stream()
+			.collect(Collectors.groupingBy(Anime::getOrigen,Collectors.collectingAndThen(Collectors.counting(),Long::intValue)));
+	}
+	
+	// Un método que devuelva un Map en el que las claves sean un atributo o una función sobre un atributo, y los valores son máximos/mínimos de los elementos que tienen ese valor.
 
+	public Map<String,Double> mayorValoracionObtenidaPorEstudio(){
+		Comparator<Anime> cp = Comparator.comparing(Anime::getValoracion);
+				
+		return animes.stream()
+				.collect(
+						Collectors.groupingBy(
+								a->a.getEstudio().nombre(),
+								Collectors.collectingAndThen(
+										Collectors.maxBy(cp), 
+										opt->opt.get().getValoracion()
+										)
+								)
+						);
+	}
+	
+	// Un método que devuelva un SortedMap en el que las claves sean un atributo o una función sobre un atributo, y los valores sean listas con los n mejores 
+	// o peores elementos que comparten el valor de ese atributo (o función sobre el atributo).
+	
+	public SortedMap<String,List<Anime>> topNAnimesMasPopularesPorEstudio(Integer n){
+		
+		return animes.stream()
+				.collect(
+						Collectors.groupingBy(
+								a->a.getEstudio().nombre(),
+								TreeMap::new,
+								Collectors.collectingAndThen(
+										Collectors.toList(),
+										l -> topNAnimesMasPopulares(l,n))
+								)
+						);
+		
+	}
+	
+	private List<Anime> topNAnimesMasPopulares(List<Anime> l, Integer n){
+		Comparator<Anime> cp = Comparator.comparing(Anime::getPopularidad).reversed();
+		return l.stream()
+				.sorted(cp)
+				.limit(n)
+				.collect(Collectors.toList());
+	}
+	
+	// Un método que calcule un Map y devuelva la clave con el valor asociado (mayor o menor) de todo el Map.
+	
+	public Studio estudioMayorPopularidadAcumulada() {
+		Map<Studio,Double> aux = popularidadAcumuladaPorEstudio();
+		return aux.entrySet().stream()
+				.max(Map.Entry.comparingByValue())
+				.get()
+				.getKey();
+	}
+	
+	private Map<Studio,Double> popularidadAcumuladaPorEstudio(){
+		return animes.stream()
+				.collect(Collectors.groupingBy(
+						a->a.getEstudio(),
+						Collectors.summingDouble(Anime::getPopularidad)
+						));
+	}
+	
+	
+	
+	
+	
+	
 }
